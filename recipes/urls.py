3d18848d652cf627fccbdd839bb02f208c078bdc
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-05-23 21:39:24 -0600

    connected recipe detail view to recipe main view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import home, RecipeListView, RecipeDetailView
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', home),
     path('list/', RecipeListView.as_view(), name='list'),
