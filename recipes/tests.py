c7682d93b479c074f748c7ed31c46ec9faf27fa3
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-06-02 16:50:22 -0600

    adding form tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
 from .models import Recipe
+from .forms import RecipeSearchForm
 
 # Tests to run:
 # name value and max length
 # cooking_time value
 # ingredients value and max length
-# difficulty value, max length, and default (later on will test correct calculation)
 # rating value, max length, and default
 # __str__ function
+
 class RecipeModelTest(TestCase):
     def setUpTestData():
         Recipe.objects.create(
             name='Hot Chocolate',
             cooking_time='5',
             ingredients='milk, hot chocolate mix, marshmallows',
-            difficulty='easy',
             rating='yum'
         )
 
@@ -43,21 +43,6 @@ class RecipeModelTest(TestCase):
         recipe_ingredients_max_length = recipe._meta.get_field('ingredients').max_length
         self.assertEqual(recipe_ingredients_max_length, 255)
     
-    def test_recipe_difficulty(self):
-        recipe = Recipe.objects.get(id=1)
-        recipe_difficulty = recipe._meta.get_field('difficulty').verbose_name
-        self.assertEqual(recipe_difficulty, 'difficulty')
-
-    def test_recipe_difficulty_max_length(self):
-        recipe = Recipe.objects.get(id=1)
-        recipe_difficulty_max_length = recipe._meta.get_field('difficulty').max_length
-        self.assertEqual(recipe_difficulty_max_length, 12)
-    
-    def test_recipe_difficulty_default(self):
-        recipe = Recipe.objects.get(id=1)
-        recipe_difficulty_default = recipe._meta.get_field('difficulty').default
-        self.assertEqual(recipe_difficulty_default, 'na')
-    
     def test_recipe_rating(self):
         recipe = Recipe.objects.get(id=1)
         recipe_rating = recipe._meta.get_field('rating').verbose_name
@@ -80,3 +65,32 @@ class RecipeModelTest(TestCase):
     def test_get_absolute_url(self):
         recipe = Recipe.objects.get(id=1)
         self.assertEqual(recipe.get_absolute_url(), '/recipes/list/1')
+    
+class RecipeFormTest(TestCase):
+    def setUpTestData():
+        Recipe.objects.create(
+            name='Hot Chocolate',
+            cooking_time='5',
+            ingredients='milk, hot chocolate mix, marshmallows',
+            rating='yum'
+        )
+
+# Value of field
+# Format of field
+# Length of field
+# protected view
+
+    def test_form_input_value(self):
+        recipe = Recipe.objects.get(id=1)
+        recipe_name = recipe._meta.get_field('name').verbose_name
+        form = RecipeSearchForm()
+        form_value = form.get(recipe_name)
+        self.assertEqual(form_value, recipe_name)
+
+
+    # def test_form_input_format:
+    
+    # def test_form_input_max_length:
+    #     recipe = Recipe.objects.get(id=1)
+    #     recipe_rating_max_length = recipe._meta.get_field('rating').max_length
+    #     self.assertEqual(recipe_rating_max_l
