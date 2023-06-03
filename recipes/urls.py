549b173cabb4c3b0b3e563d5e44abd4d21ad6cd9
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-06-03 15:30:21 -0600

    debugging

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,7 @@ from .views import home, RecipeListView, RecipeDetailView, search, about
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('/', home),
     path('about/', about, name='about'),
     path('list/', RecipeListView.as_view(), name='list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
