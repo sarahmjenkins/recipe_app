6f2b8b02126dea587d74df8d5cd0ad7724021948
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-06-03 15:26:18 -0600

    Update ALLOWED_HOSTS with site and development server URL

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://intense-stream-67991.herokuapp.com/']
 
 
 # Application definition
